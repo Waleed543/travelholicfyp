6850cfc689d3bfd8424d984c86f5b8d1ad69f169
Waleed543 <47062175+Waleed543@users.noreply.github.com>
2020-10-21 20:54:46 +0500

    profile and payment front end
    sss

---
+++ resources/views/payment.blade.php
@@ -0,0 +1,96 @@
+@extends('layouts.app')
+@section('nav','fixed-top')
+@section('content')
+
+    <div class="container" style="margin-top: 10rem; background-color: lightgrey; height: 900px; border-radius: 20px; color: #40474e">
+
+        <div class="row">
+
+
+            <div class="col-lg-12" style="height: 800px; background-color: white; padding: 0; margin-top: 3rem; border-radius: 20px">
+                <div class="row" style="margin-top: 3rem">
+                    <div class="col-lg-12">
+                    <h4 style="text-align: center">Booking ID: 9922233</h4>
+                    </div>
+
+                </div>
+
+                <div class="row" style="margin-top: 3rem;">
+                    <div class="col-3" style="margin-left: 5rem">
+                        <label style=" "><b>Total cost:</b></label>
+                        <label>4599</label>
+                    </div>
+                    <div class="col-4" style="">
+                        <label style=" "><b>Booking Time:</b></label>
+                        <label>12-10-20</label>
+                    </div>
+
+                    <div class="col-4" style="">
+                        <label style=""><b>Valid Till:</b></label>
+                        <label>2 hours</label>
+                    </div>
+
+                </div>
+                <br>
+                <br>
+                <hr>
+                <h3 style="text-align: center">Payment Details</h3>
+                <div style="text-align: center">
+                <img src={{asset('img/epaisa.png')}}>
+                </div>
+
+                <div class="row">
+                    <div class="col-lg-2"></div>
+
+                    <div class="col-5" style="margin-left: 3.5rem">
+                        <label style=" "><b>Account Name:</b></label>
+                        <label>Zeeshan Khan</label>
+                    </div>
+
+
+                    <div class="col-4" style="">
+                        <label style=""><b>Account ID:</b></label>
+                        <label>03334545921</label>
+                    </div>
+
+                </div>
+                <p style="text-align: center;color: #ff0000;">Note: After payment enter your TRX id provided by easypaisa by SMS in the feild below to confirm payment</p>
+                <div class="row">
+                    <div class="col-lg-12">
+                    <form>
+                        <div class="row">
+                            <div class="col-lg-4" style="text-align: right"><label for="trxinput" class=" form-control-label"><h4>Trx ID:</h4></label></div>
+                        <div class="col-lg-4">
+
+                            <input type="text" data-role="trxinput" id="blood" name="trxinput" value="{{old('trxinput')}}" class="form-control @error('trxinput') is-invalid @enderror">
+                            @error('trxinput')
+                            <span class="invalid-feedback" role="alert">
+                                                         <strong>{{ $message }}</strong>
+                                                    </span>
+                        @enderror
+
+                        </div>
+                            <div class="col-lg-4">
+                                <button type="submit" class="btn btn-primary btn-sm" onkeypress="event.preventDefault();" value="submit" form="create">
+                                    Submit
+                                </button>
+
+                            </div>
+                        </div>
+
+                    </form>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+
+
+
+
+
+@endsection
--- resources/views/viewProfile.blade.php
+++ resources/views/viewProfile.blade.php
@@ -2,20 +2,84 @@
 @section('nav','fixed-top')
 @section('content')
 
-<div class="container" style="margin-top: 10rem; background-color: lightgrey; height: 900px; border-radius: 20px; color: #40474e">
+<div class="container" style="margin-top: 10rem; background-color: lightgrey; height: 700px; border-radius: 20px; color: #40474e">
 
     <div class="row">
         <div class="col-lg-3" style="height: 600px; background-color: white; padding: 0; margin-top: 3rem;margin-left: 2rem; border-radius: 20px">
             <h3 style="text-align: center; margin-top: 1rem">John Snow</h3>
-            <div style="height: 300px; overflow-y: hidden">
+            <div style="height: 300px; overflow-y: hidden;margin-bottom: 1.5rem" >
             <img style="width: 100%; margin-left: 0;" src={{asset('img/john.png')}}>
             </div>
-            <div class="row">
-            <label class="col-lg-6" style="">Gender</label>
+            <div class="row" style="margin-bottom: 0;">
+                <div class="col-lg-6">
+            <label style=" margin-left: 1.5rem"><b>Gender:</b></label>
+                </div>
+                <label class="col-lg-6">Male</label>
             </div>
 
+            <div class="row" style="">
+                <div class="col-lg-6">
+                    <label style=" margin-left: 1.5rem"><b>City:</b></label>
+                </div>
+                <label class="col-lg-6">Patoki</label>
+            </div>
         </div>
+        <div class="col-lg-8" style="height: 600px; background-color: white; padding: 0; margin-top: 3rem;margin-left: 2rem; border-radius: 20px">
+            <div class="row" style="margin-top: 3rem">
+                <div class="col-3" style="margin-left: 3.5rem">
+                    <label style=" "><b>Total Blogs:</b></label>
+                    <label>0</label>
+                </div>
+                <div class="col-3" style="">
+                    <label style=" "><b>Tours Posted:</b></label>
+                    <label>0</label>
+                </div>
 
+                <div class="col-3" style="">
+                    <label style=""><b>Hotels added:</b></label>
+                    <label>0</label>
+                </div>
+
+            </div>
+
+            <div class="row" style="margin-top: 3rem; ">
+                <div class="col-3" style="margin-left: 3.5rem">
+                    <label style=" "><b>Vehicles added:</b></label>
+                    <label>0</label>
+                </div>
+                <div class="col-3" style="">
+                    <label style=" "><b>Bookings:</b></label>
+                    <label>0</label>
+                </div>
+
+                <div class="col-4" style="">
+                    <label style=""><b>Member Since:</b></label>
+                    <label>The winter</label>
+                </div>
+
+            </div>
+            <br>
+            <br>
+            <hr>
+            <h3 style="text-align: center">Personal Info</h3>
+
+            <div class="row" style="margin-top: 3rem">
+
+                <div class="col-6" style="margin-left: 3.5rem">
+                    <label style=" "><b>Address:</b></label>
+                    <label>near attok petrol pump, Patoki</label>
+                </div>
+
+
+                <div class="col-5" style="">
+                    <label style=""><b>Phone:</b></label>
+                    <label>03334545921</label>
+                </div>
+
+            </div>
+
+
+        </div>
 
     </div>
 
