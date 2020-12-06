8a98f82d86362667018e1b6aedb6574efc8a1dea
bilalaijaz <56949610+bilalaijaz@users.noreply.github.com>
2020-12-06 22:07:52 +0500

    dd

---
+++ database/migrations/2020_12_04_152130_create_vehicles_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVehiclesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vehicles', function (Blueprint $table) {
+            $table->integerIncrements('id');
+            $table->unsignedInteger('user_id');
+            $table->string('slug',100)->unique();
+            $table->string('name', 100);
+            $table->mediumText('description');
+            $table->date('year');
+            $table->string('make', 100);
+            $table->string('model', 100);
+            $table->string('color', 100);
+            $table->string('condition', 100);
+            $table->integer('mileage');
+            $table->string('VINumber', 100);
+            $table->unsignedInteger('city');
+            $table->string('thumbnail');
+            $table->string('status',100);
+            $table->softDeletes();
+            $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('city')->references('id')->on('city');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vehicles');
+    }
+}
